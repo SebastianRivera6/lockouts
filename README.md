2472fb2957197b4a127edb223ec76fc6ec2db0ee
SebastianRivera6 <Sebastian_river6@csu.fullerton.edu>
2023-12-21 07:36:17 -0800

    completed openpyxl integration and first POST request setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # lockouts
-lockouts
+This is a program that utilizes openpyxl to grab unique IDs from a file directory and append it to a the body of a POST request to interact wit Starrez
--- main.py
+++ main.py
@@ -1,65 +1,86 @@
-import os.path
+import json
+import requests
+import openpyxl
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+def get_most_recent_entry(file_path, sheet_name, column_name):
+    try:
+        workbook = openpyxl.load_workbook(file_path)
+        sheet = workbook[sheet_name]
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+        # Assuming the entries are in column 'A' and have headers in the first row
+        column_values = [cell.value for cell in sheet[column_name] if cell.value is not None]
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = ""
-SAMPLE_RANGE_NAME = "Class Data!A:D"
+        # Get the most recent entry
+        most_recent_entry = column_values[-1] if column_values else None
+
+        return most_recent_entry
+
+    except Exception as e:
+        print("Error reading Excel file:", str(e))
+        return None
 
 
-def main():
-  """Shows basic usage of the Sheets API.
-  Prints values from a sample spreadsheet.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+def send_post_request(request_data, variable_value=None):
+    try:
+        # Parse the JSON request data
+        request_json = json.loads(request_data)
 
-  try:
-    service = build("sheets", "v4", credentials=creds)
+        # Extract parameters from the JSON data
+        uri = request_json.get("uri")
+        method = request_json.get("method", "POST")
+        headers = request_json.get("headers", {})
+        authentication = request_json.get("authentication", {})
+        body = request_json.get("body", "")
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = (
-        sheet.values()
-        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
-        .execute()
-    )
-    values = result.get("values", [])
+        # Replace the variable in the body with the most recent entry from the Excel file
+        if variable_value is None:
+            variable_value = get_most_recent_entry("../../../Dropbox (CSU Fullerton)/Admin and Conference Services/Technology/Lockouts/Lockouts.xlsx", "Lockouts List", "A")
 
-    if not values:
-      print("No data found.")
-      return
+        # Prepare the authentication details
+        auth_type = authentication.get("type", "")
+        username = authentication.get("username", "")
+        password = authentication.get("password", "")
 
-    print("Name, Major:")
-    for row in values:
-      # Print columns A and E, which correspond to indices 0 and 4.
-      print(f"{row[0]}, {row[4]}")
-  except HttpError as err:
-    print(err)
+        # Prepare the request headers
+        headers["Content-Type"] = "application/json"
 
+        # Prepare the request
+        response = requests.request(
+            method,
+            uri,
+            headers=headers,
+            auth=(username, password) if auth_type.lower() == "basic" else None,
+            data=body.replace('{variable}', str(variable_value)) if body else None
+
+        )
+
+        # Print the response
+        print("Response Code:", response.status_code)
+        print("Response Content:", response.text)
+
+    except json.JSONDecodeError as e:
+        print("Error decoding JSON:", str(e))
+    except Exception as e:
+        print("An error occurred:", str(e))
 
 if __name__ == "__main__":
-  main()
+    # Example JSON data with a variable placeholder '{cwid}'
+    example_json = '''
+    {
+        "uri": "https://fullerton.starrezhousing.com/StarRezREST/services/query",
+        "method": "POST",
+        "headers": {
+            "Accept": "application/json"
+        },
+        "authentication": {
+            "username": "housingrest@fullerton.edu",
+            "password": "1bfb9011-6989-4d34-9eec-238e224ab253",
+            "type": "Basic"
+        },
+        "body": "select namepreferred, namefirst, namelast, entryid from entry where id1='{variable}'"
+    }
+    '''
+    #variable_value = get_most_recent_entry("Lockouts.xlsx", "Lockouts List", "A")
+
+    # Send the POST request using the example JSON data and variable value
+    send_post_request(example_json)
