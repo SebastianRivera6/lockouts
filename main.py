3e7d6b40c38d716ce491147877a23570b756f091
SebastianRivera6 <42875604+SebastianRivera6@users.noreply.github.com>
2024-04-02 12:01:53 -0700

    commited to new Beta Version
    removed alpha version selection option

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from functions import *
-from functionsBeta import *
 
 if __name__ == "__main__":
-
     print("Welcome!\n")
-    Version = input("Would you like to run Version 1 or 2:  ")
-    
-    if Version==1:
-        AddGenericData()
-    else:
-        Driver()
+    Driver()
         
